30c48e592456c8f04b66aa1410dd9a36e3297e71
Bangalter <estebamlo1975@gmail.com>
2022-08-24 17:17:37 -0300

    Added a paragraph with DOM

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const d = document;
 const body = document.body;
 
 const $title = document.createElement("h1");
-$title.textContent = "Second Branch";
+$title.textContent = "Hello World";
 
 body.appendChild($title);
+
+const mainContent = document.createElement("main");
+mainContent.innerHTML = "<p>This is a paragraph</p>";
+body.appendChild(mainContent);
